450a8da5e8def9bb596581a4fc73afd069ba31d3
Gohan-PC <nahoj1992@gmail.com>
2019-02-03 20:15:02 -0500

    version1.5.1

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ require_once 'controllers/usuario.php';
 
 $controlador = new UsuarioController();
 
-if(isset($_GET['action'])){
+if(isset($_GET['action']) && method_exists($controlador,$_GET['action'])){
     $action = $_GET['action'];
     $controlador->$action();
+}else{
+    echo"La pagina que buscas no existe";
 }
