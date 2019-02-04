fc7488ba941940724bfd1ef1c99d5b9c91f02e40
Gohan-PC <nahoj1992@gmail.com>
2019-02-03 20:36:31 -0500

    version1.5.1

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 
 require_once 'controllers/usuario.php';
 
-$controlador = new UsuarioController();
 
-if(isset($_GET['action']) && method_exists($controlador,$_GET['action'])){
-    $action = $_GET['action'];
-    $controlador->$action();
+if(isset($_GET['controller']) && class_exists($_GET['controller'])){
+    $name_controller = $_GET['controller'];
+    $controlador = new $name_controller();
+
+    if(isset($_GET['action']) && method_exists($controlador,$_GET['action'])){
+        $action = $_GET['action'];
+        $controlador->$action();
+    }else{
+        echo"La action que buscas no existe";
+    }
 }else{
     echo"La pagina que buscas no existe";
 }
