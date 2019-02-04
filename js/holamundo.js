9c3d65c0db28a0ca501b6d610d631846b3c3548f
Gohan-PC <nahoj1992@gmail.com>
2019-02-03 19:27:58 -0500

    version1.2

---
+++ js/holamundo.js
@@ -0,0 +1,11 @@
+/*alert('en otro directorio')
+alert("hola mundo ");
+alert("uno ");
+alert("dos");
+alert("tres");
+
+document.write("escribiendo desde un archivo js");
+
+console.log(88+1);*/
+
+var pais = "peru";
